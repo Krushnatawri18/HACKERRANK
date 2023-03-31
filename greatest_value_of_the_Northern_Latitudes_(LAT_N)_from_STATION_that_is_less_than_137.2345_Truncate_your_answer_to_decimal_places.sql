807ba68aee47174fc5d8073b95e77f34ee35283a
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2023-03-31 14:38:16 +0530

    Update greatest_value_of_the_Northern_Latitudes_(LAT_N)_from_STATION_that_is_less_than_137.2345_Truncate_your_answer_to_decimal_places.sql

--- greatest_value_of_the_Northern_Latitudes_(LAT_N)_from_STATION_that_is_less_than_137.2345_Truncate_your_answer_to_decimal_places.sql
+++ greatest_value_of_the_Northern_Latitudes_(LAT_N)_from_STATION_that_is_less_than_137.2345_Truncate_your_answer_to_decimal_places.sql
@@ -1,5 +1,7 @@
 /*
 Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than . Truncate your answer to  decimal places.
+
+Subquery is used
 */
 
 SELECT ROUND(MAX(LAT_N),4) FROM STATION WHERE LAT_N IN(SELECT LAT_N FROM STATION WHERE LAT_N < 137.2345);
