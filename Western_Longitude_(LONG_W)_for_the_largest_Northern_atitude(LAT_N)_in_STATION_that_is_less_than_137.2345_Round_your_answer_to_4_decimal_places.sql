db9c9d5c4435c3017e77cece118cda28295586de
Krushna Tawri <116067639+Krushnatawri18@users.noreply.github.com>
2023-03-31 14:51:38 +0530

    Update Western_Longitude_(LONG_W)_for_the_largest_Northern_atitude(LAT_N)_in_STATION_that_is_less_than_137.2345_Round_your_answer_to_4_decimal_places.sql

--- Western_Longitude_(LONG_W)_for_the_largest_Northern_atitude(LAT_N)_in_STATION_that_is_less_than_137.2345_Round_your_answer_to_4_decimal_places.sql
+++ Western_Longitude_(LONG_W)_for_the_largest_Northern_atitude(LAT_N)_in_STATION_that_is_less_than_137.2345_Round_your_answer_to_4_decimal_places.sql
@@ -1,5 +1,5 @@
 /*
-Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
 
 Subquery is used here.
 */
